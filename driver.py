cf2585b6fc5b93bb2230d7588cdcd86ea1b29538
Matthew-Beller <mrbeller@verizon.net>
2022-05-02 01:36:46 -0400

    Changed tkinter to gooey

--- driver.py
+++ driver.py
@@ -1,9 +1,31 @@
 import srt
 import os
-import tkinter as tk
-import tkinter.filedialog
-import tkinter.simpledialog
+from gooey import Gooey, GooeyParser
 from difflib import SequenceMatcher
+
+#Use gooey
+
+@Gooey
+def parse_args():
+    parser = GooeyParser(description="Speeder") 
+    parser.add_argument('source_directory_video', widget="DirChooser")
+    parser.add_argument('source_directory_subtitle', widget="DirChooser")
+    parser.add_argument('key_phrase', widget="Textarea")
+    return parser.parse_args()
+
+args = parse_args()
+
+source_directory_video = args.source_directory_video
+
+source_directory_subtitle = args.source_directory_subtitle
+
+key_phrase = args.key_phrase
+
+
+os.chdir(source_directory_video)
+
+
+subtitle_not_found_list = []
 
 def findSubtitleFile(source_directory_subtitle, video_file_name):
     os.chdir(source_directory_subtitle)
@@ -11,18 +33,9 @@ def findSubtitleFile(source_directory_subtitle, video_file_name):
         if(subtitle_file_name.lower().endswith(".srt")):
             if(SequenceMatcher(None, subtitle_file_name, video_file_name).ratio() > 0.75):
                 source_text = open(subtitle_file_name, "r")
+                print(subtitle_file_name + " " + video_file_name)
                 return source_text
     return None
-
-application_window = tk.Tk()
-
-source_directory_video = tkinter.filedialog.askdirectory()
-
-source_directory_subtitle = tkinter.filedialog.askdirectory()
-
-os.chdir(source_directory_video)
-
-key_phrase = tkinter.simpledialog.askstring("Input", "Enter your search phrase:", parent = application_window)
 
 for video_file_name in os.listdir(source_directory_video):
     subtitle_file = findSubtitleFile(source_directory_subtitle, video_file_name)
@@ -33,4 +46,6 @@ for video_file_name in os.listdir(source_directory_video):
 
         for entry in subtitles_list:
             if(entry.content.find(key_phrase) != -1):
-                print(entry.content) 
+                print(entry.content)
+    else:
+        subtitle_not_found_list.append(video_file_name)
