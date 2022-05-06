4d60b450a0571b7e7a6a0724c7c30f86fb12a8bc
Matthew-Beller <mrbeller@verizon.net>
2022-05-06 17:59:23 -0400

    Added support for single file searches

--- driver.py
+++ driver.py
@@ -42,6 +42,8 @@ if(args.action == 'Single'):
     key_phrase_clean = ''.join(filter(str.isalnum, key_phrase))
     key_phrase_clean = key_phrase_clean.lower()
 
+    save_style_num = 4
+
 elif(args.action == 'Batch'):
     source_directory_video = args.source_directory_video
     ignore_video = args.ignore_video
@@ -132,26 +134,32 @@ def find_video_matches(intial_directory, output_directory, video_subfolder):
         if(os.path.isfile(os.path.join(intial_directory, video_file_name))):
             subtitle_file = find_subtitle_file(source_directory_subtitle, video_file_name)
             if(subtitle_file != None):
-                if(save_style_num == 2):
-                    found_matches_file_name = video_file_name + ".txt"
-                elif(save_style_num == 0):
-                    found_matches_file_name = os.path.basename(source_directory_video) + ".txt"
-                else:
-                    found_matches_file_name = os.path.basename(intial_directory) + ".txt"
+                found_matches_file = find_output_file(video_file_name, intial_directory)
+
                 os.chdir(output_file_directory)
-                try:
-                    found_matches_file = open(found_matches_file_name,"x")
-                except:
-                    if(save_style_num == 2):
-                        print(found_matches_file_name + "already exists.")
 
-                found_matches_file = open(found_matches_file_name,"a")
-
-                find_matching_entries(subtitle_file, key_phrase, found_matches_file)
+                find_matching_entries(subtitle_file, key_phrase_clean, found_matches_file, os.path.abspath(video_file_name))
             else:
                 subtitle_not_found_list.append(video_file_name)
 
-def find_matching_entries(subtitle_file, key_phrase, found_matches_file):
+def find_output_file(video_file_name, intial_directory = None):
+    if(save_style_num == 2 or save_style_num == 4):
+        found_matches_file_name = video_file_name + ".txt"
+    elif(save_style_num == 0):
+        found_matches_file_name = os.path.basename(source_directory_video) + ".txt"
+    else:
+        found_matches_file_name = os.path.basename(intial_directory) + ".txt"
+    try:
+        found_matches_file = open(found_matches_file_name,"x")
+    except:
+        if(save_style_num == 2):
+            print(found_matches_file_name + "already exists.")
+    found_matches_file = open(found_matches_file_name,"a")
+
+    return found_matches_file
+
+
+def find_matching_entries(subtitle_file, key_phrase, found_matches_file, video_file_path):
     with open(subtitle_file, encoding='utf-8-sig') as file:
         subtitle_generator = srt.parse(file)
 
@@ -160,8 +168,12 @@ def find_matching_entries(subtitle_file, key_phrase, found_matches_file):
     for entry in subtitles_list:
         entry_clean = ''.join(filter(str.isalnum, entry.content)) 
         entry_clean = entry_clean.lower()
+        key_phrase_clean = ''.join(filter(str.isalnum, key_phrase))
+        key_phrase_clean = key_phrase_clean.lower()
         if(entry_clean.find(key_phrase_clean) != -1):
-            found_matches_file.write(entry.content)
+            found_matches_file.write(video_file_path + "\n")
+            found_matches_file.write(str(entry.start) + " --> " + str(entry.end) + "\n")
+            found_matches_file.write(entry.content + "\n\n")
 
     found_matches_file.close()
 
@@ -169,7 +181,9 @@ def find_matching_entries(subtitle_file, key_phrase, found_matches_file):
 #TODO: turn code into more functions
 
 if(args.action == 'Single'):
-    print("placeholder for finding entries of single file")
+    os.chdir(output_directory)
+    output_file = find_output_file(os.path.basename(source_file_video))
+    find_matching_entries(source_file_subtitle, key_phrase, output_file, source_file_video)
 elif(args.action == 'Batch'):
     directory_found = False
 
